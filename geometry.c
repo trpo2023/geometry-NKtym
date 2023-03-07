4b05b5a656cc692ce325747aa164007a979584fe
Pavel Gorodilov <NKtym@users.noreply.github.com>
2023-03-07 11:22:36 +0700

    Rename macros geometry.c

--- geometry.c
+++ geometry.c
@@ -1,8 +1,10 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#define NULLASCII 48
+
+#define ZEROASCII 48
 #define NINEASCII 57
+
 int Figur(char* str)
 {
     int flag = 1;
@@ -46,7 +48,7 @@ int Arg(char* str)
         }
     }
     for (int i = data + 1; i < strlen(str) - 2; i++) {
-        if ((str[i] >= NULLASCII && str[i] <= NINEASCII) || str[i] == '.' || str[i] == ','
+        if ((str[i] >= ZEROASCII && str[i] <= NINEASCII) || str[i] == '.' || str[i] == ','
             || str[i] == ' ')
             flag = 0;
         else {
@@ -55,12 +57,12 @@ int Arg(char* str)
         }
     }
     for (int i = data + 1; i < strlen(str) - 1; i++) {
-        if (str[i] == '.' && ((str[i + 1] < NULLASCII || str[i + 1] > NINEASCII)
-            || (str[i - 1] < NULLASCII || str[i - 1] > NINEASCII)))
+        if (str[i] == '.' && ((str[i + 1] < ZEROASCII || str[i + 1] > NINEASCII)
+            || (str[i - 1] < ZEROASCII || str[i - 1] > NINEASCII)))
             flag = 1;
         else if (
                 str[i] == ',' && (str[i + 1] != ' ')
-                && (str[i - 1] < NULLASCII && str[i - 1] > NINEASCII))
+                && (str[i - 1] < ZEROASCII && str[i - 1] > NINEASCII))
             flag = 1;
     }
     return flag;
@@ -77,7 +79,7 @@ int num(char* str){
     }
     int cnt=0;
     for (int i = data + 1; i < strlen(str) - 2; i++) {
-	if(str[i] >= NULLASCII && str[i] <= NINEASCII)
+	if(str[i] >= ZEROASCII && str[i] <= NINEASCII)
 	    cnt++;
 	else if(str[i]=='.')
 	    cnt--;
@@ -114,7 +116,7 @@ int flout(char* str){
     }
     int cnt=0;
     for (int i = data + 1; i < strlen(str) - 2; i++) {
-	if(str[i]>=NULLASCII && str[i]<=NINEASCII){
+	if(str[i]>=ZEROASCII && str[i]<=NINEASCII){
 	    for(int j=i;str[j]!=' ' && str[j]!=')' && str[j]!=',';j++){
 		if(str[j]=='.')
 		    cnt++;
